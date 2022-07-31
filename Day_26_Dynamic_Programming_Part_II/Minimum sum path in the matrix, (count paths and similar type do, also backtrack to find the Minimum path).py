44d0ca9434cd893e91f6096566611f8be3d8ed09
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-07-31 19:07:28 +0530

    Update Minimum sum path in the matrix, (count paths and similar type do, also backtrack to find the Minimum path).py

--- Day_26_Dynamic_Programming_Part_II/Minimum sum path in the matrix, (count paths and similar type do, also backtrack to find the Minimum path).py
+++ Day_26_Dynamic_Programming_Part_II/Minimum sum path in the matrix, (count paths and similar type do, also backtrack to find the Minimum path).py
@@ -1,9 +1,8 @@
 #=================================================
 #        64. Minimum Path Sum
 #https://leetcode.com/problems/minimum-path-sum/
-#
 #======================================================
-#Method-1
+#Method-1(Recursive)
 import math
 class Solution:
     def solve(self,mat,i,j,m,n):
@@ -13,4 +12,25 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m=len(grid[0])
         n=len(grid)
+        return self.solve(grid,0,0,m,n)
+
+#T-O(n*m)
+#S-O(n*m)
+#Method-2(Memorization)
+dp=[]
+import math
+class Solution:
+    def solve(self,mat,i,j,m,n):
+        global dp
+        if i==n-1 and j==m-1:return mat[n-1][m-1]
+        if i>=n or m<=j:return math.inf-200*100
+        if dp[i][j]!=-1:return dp[i][j]
+        dp[i][j]=min(mat[i][j]+self.solve(mat,i+1,j,m,n),mat[i][j]+self.solve(mat,i,j+1,m,n))
+        return dp[i][j]
+
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m=len(grid[0])
+        n=len(grid)
+        global dp
+        dp=[[-1 for i in range(m+1)] for _ in range(n+1)]
         return self.solve(grid,0,0,m,n)
