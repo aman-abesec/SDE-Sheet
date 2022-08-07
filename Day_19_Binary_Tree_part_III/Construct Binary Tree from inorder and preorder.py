05b9de176f681943289c7cd9498dd6508d368b41
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-08-07 09:35:07 +0530

    Update Construct Binary Tree from inorder and preorder.py

--- Day_19_Binary_Tree_part_III/Construct Binary Tree from inorder and preorder.py
+++ Day_19_Binary_Tree_part_III/Construct Binary Tree from inorder and preorder.py
@@ -1,5 +1,25 @@
 #=============================================
 #   105. Construct Binary Tree from Preorder and Inorder Traversal
 #  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
-#
 #============================================
+
+#T-O(n^2)
+preIndex=0
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def solve(Pre,In,s,e):
+            if s>e:return None
+            global preIndex
+            root=TreeNode(Pre[preIndex])
+            preIndex+=1
+            new_start=0
+            for i in range(s,e+1):
+                if root.val==In[i]:
+                    new_start=i
+                    break
+            root.left=solve(Pre,In,s,new_start-1)
+            root.right=solve(Pre,In,new_start+1,e)
+            return root
+        global preIndex
+        preIndex=0
+        return solve(preorder,inorder,preIndex,len(preorder)-1)
