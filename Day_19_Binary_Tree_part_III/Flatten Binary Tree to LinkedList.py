3420e80e492d0441a04e7cd9eb46639adb5a8848
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-08-07 11:40:02 +0530

    Update Flatten Binary Tree to LinkedList.py

--- Day_19_Binary_Tree_part_III/Flatten Binary Tree to LinkedList.py
+++ Day_19_Binary_Tree_part_III/Flatten Binary Tree to LinkedList.py
@@ -1,4 +1,21 @@
 #=============================================
 #   114. Flatten Binary Tree to Linked List
 #   https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+#https://youtu.be/sWf7k1x9XR4
 #============================================
+
+prev=None
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        def solve(r):
+            global prev
+            if r==None:return
+            solve(r.right)
+            solve(r.left)
+            r.right=prev
+            r.left=None
+            prev=r
+        global prev
+        prev=None
+        solve(root)
+        return prev
