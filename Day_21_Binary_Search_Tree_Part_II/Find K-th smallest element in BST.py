2721a55344a957bdc1733093e21ebbc3ebd72bb6
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-08-11 09:12:22 +0530

    Add files via upload

---
+++ Day_21_Binary_Search_Tree_Part_II/Find K-th smallest element in BST.py
@@ -0,0 +1,18 @@
+#=====================================================
+#230. Kth Smallest Element in a BST
+#https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+#======================================================
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def solve(root,c,k):
+            if root==None:return True
+            solve(root.left,c,k)
+            c[0]+=1
+            if c[0]==k:
+                c[1]=root
+                return
+            solve(root.right,c,k)
+        c=[0,None]
+        solve(root,c,k)
+        return c[1].val
