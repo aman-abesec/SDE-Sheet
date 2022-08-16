1547ac343373b0983c11da238e2d257b44f60d17
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-08-16 09:31:10 +0530

    Add files via upload

--- Day_24_Graph_Part_II/MST using Prim’s Algo.py
+++ Day_24_Graph_Part_II/MST using Prim’s Algo.py
@@ -1,4 +1,23 @@
 #=====================================================
-#Distance from the Source (Bellman-Ford Algorithm)
-#https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/0?fbclid=IwAR2_lL0T84DnciLyzMTQuVTMBOi82nTWNLuXjUgahnrtBgkphKiYk6xcyJU
+#Minimum Spanning Tree
+#https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1
 #================================================================
+
+import math
+import heapq
+class Solution:
+    def spanningTree(self, V, adj):
+        minheap=[]
+        heapq.heappush(minheap,[0,0])
+        result=0
+        visit=set()
+        # visit.add(0)
+        while minheap:
+            wt,u=heapq.heappop(minheap)
+            if u not in visit:
+                visit.add(u)
+                result+=wt
+                for v in adj[u]:
+                    if  v[0] not in visit:
+                        heapq.heappush(minheap,[v[1],v[0]])
+        return result
