70c1e28d44d23250f7ca86994bd6a12e5ee5809a
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-08-20 08:35:07 +0530

    Update LCA in Binary Tree.py

--- Day_18_Binary_Tree_part_II.py/LCA in Binary Tree.py
+++ Day_18_Binary_Tree_part_II.py/LCA in Binary Tree.py
@@ -17,3 +17,18 @@ class Solution:
         if l!=None and r!=None:return root
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         return self.solve(root,p,q)
+    
+    
+#Gfg Soluation
+class Solution:
+    #Function to return the lowest common ancestor in a Binary Tree.
+    def lca(self,root, n1, n2):
+        def solve(root,n1,n2):
+            if root==None or root.data==n1 or root.data==n2:
+                return root
+            l=solve(root.left,n1,n2)
+            r=solve(root.right,n1,n2)
+            if l!=None and r!=None:return root
+            if l==None:return r
+            if r==None:return l
+        return solve(root,n1,n2)
