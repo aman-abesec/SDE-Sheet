a7ebc89bb6e9b6335ec2c6e9e3542c6139b8333f
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-09-19 11:49:24 +0530

    Update Clone a graph (Not that easy as it looks).py

--- Day_23_Graph/Clone a graph (Not that easy as it looks).py
+++ Day_23_Graph/Clone a graph (Not that easy as it looks).py
@@ -1,4 +1,28 @@
 #=====================================================
 #133. Clone Graph
 #https://leetcode.com/problems/clone-graph/om-data-stream/
+#https://www.youtube.com/watch?v=xZ9st62u2Yk&ab_channel=Codebix
 #================================================================
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node==None:return None
+        q=deque()
+        hash_map={}
+        q.append(node)
+        hash_map[node.val]=Node(node.val)
+        while q:
+            n=q.popleft()
+            for data in n.neighbors:
+                if data.val not in hash_map:
+                    hash_map[data.val]=Node(data.val)
+                    q.append(data)
+                hash_map[n.val].neighbors.append(hash_map[data.val])
+        return hash_map[node.val]
