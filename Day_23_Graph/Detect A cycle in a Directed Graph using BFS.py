2d241d9d8d5df02fbe2783bcff290b96d3786337
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2022-09-19 13:06:00 +0530

    Update Detect A cycle in a Directed Graph using BFS.py

--- Day_23_Graph/Detect A cycle in a Directed Graph using BFS.py
+++ Day_23_Graph/Detect A cycle in a Directed Graph using BFS.py
@@ -25,3 +25,30 @@ class Solution:
             count+=1
         if count!=V:return True
         return False
+
+    #=======================================================================
+    #Course Schedule
+    #https://leetcode.com/problems/course-schedule/submissions/
+    #======================================================================
+    class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList=[[] for _ in range(numCourses)]
+        for k in prerequisites:
+            adjList[k[0]].append(k[1])
+        indegree=[0 for _ in range(numCourses)]
+        for i in range(numCourses):
+            for k in adjList[i]:
+                indegree[k]+=1
+        q=deque()
+        for i in range(numCourses):
+            if indegree[i]==0:q.append(i)
+        count=0
+        while q:
+            u=q.popleft()
+            for v in adjList[u]:
+                indegree[v]-=1
+                if indegree[v]==0:
+                    q.append(v)
+            count+=1
+        return  numCourses==count
+        
