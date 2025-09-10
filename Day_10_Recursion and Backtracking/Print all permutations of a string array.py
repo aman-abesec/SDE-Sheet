906268ffdcc5e326209c93ccb471245f7120953c
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2025-09-10 23:50:18 +0530

    Update Print all permutations of a string array.py

--- Day_10_Recursion and Backtracking/Print all permutations of a string array.py
+++ Day_10_Recursion and Backtracking/Print all permutations of a string array.py
@@ -18,3 +18,24 @@ class Solution:
                     num[ind],num[i]=num[i],num[ind]
         allpermute(nums)
         return result
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        l=len(nums)
+        def solve(memory,ans):
+            if len(ans) == l:
+                result.append(ans[:])
+                return
+            else:
+                for i in range(l):
+                    if memory[i]== 'F':
+                        ans.append(nums[i])
+                        memory[i]='T'
+                        solve(memory,ans)
+                        ans.pop()
+                        memory[i]='F'
+        memory=['F' for _ in range(l)]
+        ans=[]
+        solve(memory,ans)
+        return result
