a3efe68a2de37c6be48f0524b9d68d0a71c37a78
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-07 00:25:47 +0530

    .

--- Day_4_Arrays/Longest Consecutive Sequence.py
+++ Day_4_Arrays/Longest Consecutive Sequence.py
@@ -7,16 +7,13 @@
 # S:O(n)
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        hash_set=set()
-        for i in nums:
-            hash_set.add(i)
-        mx=0
-        for j in range(len(nums)):
-            if nums[j]-1 not in hash_set:
-                curr=nums[j]
-                l=1
-                while curr+1 in hash_set:
-                    curr+=1
-                    l+=1
-                mx=max(mx,l)
-        return mx
+        seen = set(nums)
+        ans = 0
+        for i in seen:
+            if i-1 not in seen:
+                d,c=i,1
+                while d+1 in seen:
+                    d+=1
+                    c+=1
+                ans = max(ans,c)
+        return ans
