aad8091f84be55adcc7cadfaef1e4d4b022a7297
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-07 00:08:17 +0530

    updated

--- Day_4_Arrays/4-sum-Problem.py
+++ Day_4_Arrays/4-sum-Problem.py
@@ -7,24 +7,19 @@
 # S-O(n)
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = set()
+        l = len(nums)
         nums.sort()
-        result=set()
-        for i in range(len(nums)):
-            if nums[i]>0 and nums[i]>target:
-                break
-            if(i>0 and nums[i]==nums[i-1]):
-                continue
-            for j in range(i+1,len(nums)):
-                beg=j+1
-                end=len(nums)-1
+        for i in range(l):
+            for j in range(i+1,l):
+                beg = j+1
+                end = l-1
                 while beg<end:
-                    sm=nums[i]+nums[j]+nums[beg]+nums[end]
-                    if sm==target:
+                    t=nums[i]+nums[j]+nums[beg]+nums[end]
+                    if target == t:
                         result.add((nums[i],nums[j],nums[beg],nums[end]))
                         beg+=1
                         end-=1
-                    elif sm<target:
-                        beg+=1
-                    else:
-                        end-=1
-        return result
+                    elif target>t:beg+=1
+                    else:end-=1
+        return list(result)
