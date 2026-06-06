a61f51360cc0b127ff1bb965bbacbee6c58cbef2
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-06 16:18:58 +0530

    wd

--- Day_2_Arrays/Merge two sorted Arrays without extra space.py
+++ Day_2_Arrays/Merge two sorted Arrays without extra space.py
@@ -1,20 +1,16 @@
-#======================================================
-#           88. Merge Sorted Array
-#  https://leetcode.com/problems/merge-sorted-array/
-#  https://youtu.be/C4oBXLr3zos
-#=========================================================
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        ptr1=m-1
-        ptr2=n-1
-        i=m+n-1
-        while ptr2>=0:
-            if ptr1>=0 and nums1[ptr1]>nums2[ptr2]:
-                nums1[i]=nums1[ptr1]
-                ptr1-=1
-                i-=1
-            else:
-                nums1[i]=nums2[ptr2]
-                i-=1
-                ptr2-=1
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        temp1 = m-1
+        temp2 = n-1
+        temp = m + n-1
+        while temp2>=0:
+            if  temp1>=0 and nums1[temp1]>nums2[temp2]:
+                nums1[temp]=nums1[temp1]
+                temp1-=1
+            else: 
+                nums1[temp]=nums2[temp2]
+                temp2-=1
+            temp-=1
