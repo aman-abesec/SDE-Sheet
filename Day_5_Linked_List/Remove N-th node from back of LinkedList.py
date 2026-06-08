fd5c11dc7908f6d131e7b59fc97830e73aa1ffb3
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-08 16:11:29 +0530

    code

--- Day_5_Linked_List/Remove N-th node from back of LinkedList.py
+++ Day_5_Linked_List/Remove N-th node from back of LinkedList.py
@@ -23,3 +23,20 @@ class Solution:
         else:
             s.next=s.next.next
         return head
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        fast = dummy
+        slow = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+
+        return dummy.next
