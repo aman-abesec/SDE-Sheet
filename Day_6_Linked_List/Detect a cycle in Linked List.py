9940598b991c0299cab4ba5923dbf1c25878dc49
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-08 23:31:26 +0530

    code

--- Day_6_Linked_List/Detect a cycle in Linked List.py
+++ Day_6_Linked_List/Detect a cycle in Linked List.py
@@ -21,3 +21,13 @@ class Solution:
             if slow==fast:
                 return True
         return False
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+            if fast==slow:return True
+        return False
