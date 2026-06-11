aee257a399458a76a5c214fcc009d6ee8b8d6a65
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-11 20:54:47 +0530

    code

--- Day_18_Binary_Tree_part_II.py/Check if the Binary tree is height-balanced or not.py
+++ Day_18_Binary_Tree_part_II.py/Check if the Binary tree is height-balanced or not.py
@@ -6,13 +6,12 @@
 #T-O(n)
 #S-O(n)
 class Solution:
-    def solve(self,root):
-        if root==None:return 0
-        l=self.solve(root.left)
-        r=self.solve(root.right)
-        if l==-1 or r==-1:return -1
-        if(abs(l-r)>1):return -1
-        return 1+max(l,r)
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if self.solve(root)==-1:return False
-        return True
+        def solve(temp):
+            if temp==None:return 0
+            l = solve(temp.left)
+            r = solve(temp.right)
+            if l==-1 or r==-1:return -1
+            if abs(l-r)>1:return -1
+            return 1+max(l,r)
+        return solve(root)!=-1
