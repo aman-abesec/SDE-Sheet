f1c59c54aa83f38f969663efa3e39a4dd358ef4c
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2026-06-13 11:40:12 +0530

    code

--- Day_19_Binary_Tree_part_III/Construct Binary Tree from Inorder and Postorder.py
+++ Day_19_Binary_Tree_part_III/Construct Binary Tree from Inorder and Postorder.py
@@ -22,3 +22,34 @@ class Solution:
         global postIndex
         postIndex=len(postorder)-1
         return solve(inorder,postorder,0,postIndex)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self,inorder: List[int],postorder: List[int]) -> Optional[TreeNode]:
+        index_map = {}
+        for i, value in enumerate(inorder):
+            index_map[value] = i
+        postIndex = len(postorder) - 1
+
+        def solve(start, end):
+            nonlocal postIndex
+
+            if start > end:return None
+
+            root_val = postorder[postIndex]
+            postIndex -= 1
+            root = TreeNode(root_val)
+            mid = index_map[root_val]
+            root.right = solve(mid + 1, end)
+            root.left = solve(start, mid - 1)
+
+            return root
+
+        return solve(0, len(inorder) - 1)
+
